264780bb39e7867a50e9926dd6e2f7a25996ff3c
64830341+JHyuna@users.noreply.github.com <64830341+JHyuna@users.noreply.github.com>
2020-12-29 18:54:31 +0900

    7568

---
+++ 1 prob each day/S5-7568.py
@@ -0,0 +1,12 @@
+t = int(input())
+a = [input().split() for _ in range(t)]
+seq = []
+len_a = len(a)
+for i in range(len_a):5
+    cnt = 1
+    for j in range(len_a):
+        if a[i][0] < a[j][0] and a[i][1] < a[j][1]:
+            cnt += 1
+    seq.append(cnt)
+for i in seq:
+    print(i, end=' ')
